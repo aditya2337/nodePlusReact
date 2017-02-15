46a2c79393794908858d2215fe64780563160761
aditya2337 <aditya.n@gai.co.in>
2017-02-15 10:13:50 +0530

    managed to switch sessions between react and node server

--- loginSessions/reactClient/src/components/Login.js
+++ loginSessions/reactClient/src/components/Login.js
@@ -3,37 +3,64 @@ import React, {Component} from 'react';
 export default class Login extends Component {
   constructor (props) {
     super(props);
-
+    this.state = {
+      isLoggedIn: true
+    };
     this.onFormSubmit = this.onFormSubmit.bind(this);
+  }
+
+  componentWillMount () {
+    fetch(`http://localhost:3001`, {
+      method: 'GET',
+      credentials: 'include'
+    })
+      .then(blob => blob.json())
+      .then(res => {
+        if (!res.authenticated) {
+          this.setState({isLoggedIn: false}); 
+        }
+        console.log(res);
+      });
   }
 
   onFormSubmit (e) {
     e.preventDefault();
     const { username, password } = this.refs;
     fetch(`http://localhost:3001/login?username=${username.value}&password=${password.value}`, {
+      credentials: 'include',
       method: 'post'
     })
       .then(blob => blob.json())
       .then(res => {
+        if (res.authenticated) {
+          this.setState({isLoggedIn: true});
+        }
         console.log(res);
       });
   }
 
   render () {
-    return (
-      <form onSubmit={this.onFormSubmit}>
-        <div>
-          <label>Email:</label>
-          <input type='text' ref='username' />
-        </div>
-        <div>
-          <label>Password:</label>
-          <input type='password' ref='password' />
-        </div>
-        <div>
-          <input type='submit' value='Log In' />
-        </div>
-      </form>
-    );
+    const {isLoggedIn} = this.state;
+
+    if (isLoggedIn) {
+      return (<p>Welcome</p>);
+
+    } else {
+      return (
+        <form onSubmit={this.onFormSubmit}>
+          <div>
+            <label>Email:</label>
+            <input type='text' ref='username' />
+          </div>
+          <div>
+            <label>Password:</label>
+            <input type='password' ref='password' />
+          </div>
+          <div>
+            <input type='submit' value='Log In' />
+          </div>
+        </form>
+      );
+    }
   }
 }
--- loginSessions/server.js
+++ loginSessions/server.js
@@ -2,16 +2,21 @@ const express = require('express');
 const session = require('express-session');
 const passport = require('passport');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const db = require('./db');
 require('./passport');
 
 express()
+  .use(cookieParser())
   .use(bodyParser.json())
   .use(bodyParser.urlencoded({extended: false}))
-  .use(session({ secret: 'i love dogs', resave: false, saveUninitialized: false }))
+  .use(session({ secret: 'i love dogs', resave: false, cookie: { httpOnly: false }, saveUninitialized: false }))
   .use(passport.initialize())
   .use(passport.session())
   .get('/', (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.json({
       session: req.session,
       user: req.user,
@@ -19,12 +24,14 @@ express()
     });
   })
   .get('/login', (req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.json({name: 'login again'});
   })
   .post('/login', (req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
   }, passport.authenticate('local', {
